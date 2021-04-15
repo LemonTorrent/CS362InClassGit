0a8f70b439ae50b7211cc8fd3a0d1ece4da55a75
LemonTorrent <nyarbrough14@gmail.com>
2021-04-15 16:48:58 -0700

    add divide method

--- calc.py
+++ calc.py
@@ -8,6 +8,9 @@ def calc(a,b):
     multiply = a * b
     print(multiply)
 
+    division = a / b
+    print(division)
+
 # write code for subtract, commit
 # write code for multiply, commit
 # write code for division, commit
