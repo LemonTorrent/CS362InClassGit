018c5c869169a61acbbfa964855b3096c15a9826
LemonTorrent <nyarbrough14@gmail.com>
2021-04-15 16:47:37 -0700

    add subtract method

--- calc.py
+++ calc.py
@@ -1,3 +1,10 @@
 def calc(a,b):
     sum = a + b
     print(sum)
+
+    difference = a - b
+    print(difference)
+
+# write code for subtract, commit
+# write code for multiply, commit
+# write code for division, commit
